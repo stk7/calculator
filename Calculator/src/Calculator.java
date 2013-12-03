5aea16bc9c5cd7c5b08db64196154a77e650f246
stk7 <stk751@gmail.com>
2013-12-03 21:52:57 +0400

    initial commit

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!!!");
+
+	}
+
+}
